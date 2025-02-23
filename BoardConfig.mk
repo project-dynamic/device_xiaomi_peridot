350ccb4cc6761c6d3ac7ae14a021c13b0c9cba08
Guidix <guidixm@gmail.com>
2025-02-23 16:46:46 +0200

    peridot: Add Fingerprint props to product
    Fixup separator for sensor position
    OSS HAL use : instead of | for separating position coordinates
    Also set correct fod position
    
    PugzAreCute: Adapt for Kenway DT
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,6 +195,7 @@ TARGET_BOARD_PLATFORM := pineapple
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
 
 # Recovery
 $(call soong_config_set, ufsbsg, ufsframework, bsg)
