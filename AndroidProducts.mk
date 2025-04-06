752ab5f29180bcdb48489737d532ab0d2ecb6b36
Your Name <you@example.com>
2025-04-06 05:45:57 +0000

    peridot: Bringup @ProjectInfinity-X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_peridot.mk
+    $(LOCAL_DIR)/infinity_peridot.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_peridot-eng \
-    aosp_peridot-userdebug \
-    aosp_peridot-user
+    infinity_peridot-eng \
+    infinity_peridot-userdebug \
+    infinity_peridot-user
