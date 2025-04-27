3ff2c7df8e77db4652379c3f65489da6eaa097fb
raghavt20 <raghavt20@aospa.co>
2025-04-27 10:00:10 +0000

    m ignore

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,30 +1,18 @@
 # Extras
 rm -rf hardware/qcom-caf/common
-git clone https://github.com/kenway214/android_hardware_qcom-caf_common hardware/qcom-caf/common --depth=1
+git clone https://github.com/poco-f6-peridot/android_hardware_qcom-caf_common hardware/qcom-caf/common --depth=1
+
+rm -rf vendor/qcom/opensource/vibrator
+git clone https://github.com/poco-f6-peridot/android_vendor_qcom_opensource_vibrator vendor/qcom/opensource/vibrator
 
 rm -rf hardware/qcom-caf/sm8650/audio/agm
-git clone https://github.com/xiaomi-peridot/vendor_qcom_opensource_agm -b vic-sm8650 hardware/qcom-caf/sm8650/audio/agm --depth=1
+git clone https://github.com/poco-f6-peridot/android_vendor_qcom_opensource_agm hardware/qcom-caf/sm8650/audio/agm --depth=1
 
 rm -rf hardware/qcom-caf/sm8650/audio/pal
-git clone https://github.com/xiaomi-peridot/vendor_qcom_opensource_arpal-lx -b vic-sm8650 hardware/qcom-caf/sm8650/audio/pal --depth=1
+git clone https://github.com/poco-f6-peridot/android_vendor_qcom_opensource_arpal-lx hardware/qcom-caf/sm8650/audio/pal --depth=1
 
 rm -rf hardware/qcom-caf/sm8650/audio/primary-hal
-git clone https://github.com/xiaomi-peridot/hardware_qcom_audio-ar -b vic-sm8650 hardware/qcom-caf/sm8650/audio/primary-hal --depth=1
-
-rm -rf hardware/qcom-caf/sm8650/data-ipa-cfg-mgr
-git clone https://github.com/xiaomi-peridot/vendor_qcom_opensource_data-ipa-cfg-mgr -b vic-sm8650 hardware/qcom-caf/sm8650/data-ipa-cfg-mgr --depth=1
-
-rm -rf hardware/qcom-caf/sm8650/dataipa
-git clone https://github.com/xiaomi-peridot/vendor_qcom_opensource_dataipa -b vic-sm8650 hardware/qcom-caf/sm8650/dataipa --depth=1
-
-rm -rf hardware/qcom-caf/sm8650/display
-git clone https://github.com/xiaomi-peridot/hardware_qcom_display -b vic-sm8650 hardware/qcom-caf/sm8650/display --depth=1
-
-rm -rf hardware/qcom-caf/sm8650/media
-git clone https://github.com/xiaomi-peridot/hardware_qcom_media -b vic-sm8650 hardware/qcom-caf/sm8650/media --depth=1
-
-rm -rf device/qcom/sepolicy_vndr/sm8650
-git clone https://github.com/xiaomi-peridot/device_qcom_sepolicy_vndr -b vic-sm8650 device/qcom/sepolicy_vndr/sm8650 --depth=1
+git clone https://github.com/poco-f6-peridot/android_hardware_qcom_audio-ar hardware/qcom-caf/sm8650/audio/primary-hal --depth=1
 
 # Device Repositories
 git clone https://github.com/peridot-hyperos-2/vendor_xiaomi_peridot vendor/xiaomi/peridot --depth=1
